1c86fe5e3923de2fe1f43bb39b0e9e243e4e1729
mohamedturki <turkipro@gmail.com>
2013-09-22 15:14:36 +0200

    switched to class based views

--- filmastest/filmastest/urls.py
+++ filmastest/filmastest/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from thumbgen.views import ThumbnailGeneratorView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
         url(r'^admin/', include(admin.site.urls)),
-        url(r'^', 'testapp.views.index', name="index"),
-        url(r'^generate/', 'thumbgen.views.generate_thumbnail', name="generate_thumbnail")        
+        url(r'^generate/', ThumbnailGeneratorView.as_view(), name="generate_thumbnail"),
+        url(r'^$', 'testapp.views.index', name="index"),
+                
 )
--- filmastest/thumbgen/views.py
+++ filmastest/thumbgen/views.py
@@ -3,7 +3,7 @@ import os
 
 from django.http import HttpResponseRedirect
 from django.conf import settings
-from django.views.decorators.cache import cache_control
+from django.views.generic.base import View
 
 from azure.storage import BlobService
 from PIL import Image
@@ -14,7 +14,6 @@ blob_service = BlobService(
 )
 
 
-@cache_control(max_age=3600)
 def generate_thumbnail(request):
     """
         Generates a thumbnail on demand
@@ -24,7 +23,7 @@ def generate_thumbnail(request):
         3- upload to Azure.
         4- return url
     """
-
+    print "dafuq"
     size = request.GET.get('size', "100x100").split("x")
     path = request.GET.get('path')
 
@@ -63,3 +62,45 @@ def generate_thumbnail(request):
     return HttpResponseRedirect(
         resized_blob.blobs[0].url
     )
+
+
+class ThumbnailGeneratorView(View):
+    def get(self, request):
+        size = request.GET.get('size', "100x100").split("x")
+        path = request.GET.get('path')
+
+        image_stream = blob_service.get_blob(
+            settings.AZURE_STORAGE_CONTAINER,
+            path
+        )
+        image = Image.open(StringIO.StringIO(image_stream))
+        image = image.resize((int(size[0]), int(size[1])), Image.ANTIALIAS)
+
+        original_filename = path.split('/')[-1]
+        resized_image_filename = '{0}_{1}_{2}'.format(
+            size[0], size[1], original_filename
+        )
+        resized_image_path = path.replace(
+            original_filename, resized_image_filename
+        )
+        image.save(resized_image_filename)
+        image_output = StringIO.StringIO()
+        image.save(image_output, format="JPEG")
+
+        blob_service.put_blob(
+            settings.AZURE_STORAGE_CONTAINER,
+            resized_image_path,
+            image_output.getvalue(),
+            x_ms_blob_type='BlockBlob',
+            x_ms_blob_content_type='image/jpeg'
+        )
+
+        resized_blob = blob_service.list_blobs(
+            settings.AZURE_STORAGE_CONTAINER, prefix=resized_image_path
+        )
+        del image
+        os.remove(resized_image_filename)
+        print resized_blob.blobs[0].url 
+        return HttpResponseRedirect(
+            resized_blob.blobs[0].url
+        )
